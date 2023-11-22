20d34c2d03afa1e3b951144f5682921e1a8c6e3a
Maria Martin <maria.martin98@educa.madrid.org>
2023-11-22 10:56:57 +0100

    buenas tardes

--- Hola2.py
+++ Hola2.py
@@ -1,2 +1,3 @@
 print('Hola Mundo')
 print('Nuevo mensaje')
+print('Linea 1 desde VCD')
--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,5 @@
 print('Hola Mundo')
+print('Nuevo mensaje')
+print('Tercer mensaje para probar el reset')
+print('hola')
+
